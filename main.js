1db2276bb0d8351b2bf41027f2fe3de66d9e6879
AdamBAKHOUCH <adam.bakhouch@esprit.tn>
2023-10-20 15:40:25 +0100

    wahoo

--- main.js
+++ main.js
@@ -1,15 +1,58 @@
 // Variables:
 
 // Problem: Write a JavaScript function that declares a variable using var and returns its value.
-
+function Variables (x){
+    var x
+return x
+}
+Variables(5)
+5
 // Problem: Create a function that takes two arguments and checks if they are equal. Return true if they are and false if they are not.
 
+function isEqual (x,y){
+    if ((x-y===0)||(y-x===0)){
+        return("true")
+
+    }
+    else {return"false"}
+    
+}
+undefined
+isEqual(5,5)
+'true'
+isEqual(5,4)
+'false'
+
 // Problem: Implement a function that declares a "constant" variable using var (even though var variables are not constant) and attempts to reassign it. Handle any resulting errors.
+function reassign(y){
+    var x=9 
+    x=y
+    return(x)
+    }
+    undefined
+
+    reassign(99)
+    99
 
 // Problem: Create a function that defines a variable using var within its scope. Try to access the variable outside of the function and explain the result.
+function withoutWithin (x) {
+    var x=5
+    return(x)
+    }
+    undefined
+    x
+    VM86:1 Uncaught ReferenceError: x is not defined
+        at <anonymous>:1:1
+    (anonymous) @ VM86:1
+    //The variable is out of the global scope reach as it is defined only in te local scope of our function.
 
 // Problem: Write a function that takes a variable as an argument and returns its data type.
+function dataType(number){
+    var number 
 
+return (typeof(number))
+
+}
 // Conditional Statements:
 
 // Problem: Create a function that takes an age as an argument and returns a message based on whether the person is a child, a teenager, an adult, or a senior citizen.
